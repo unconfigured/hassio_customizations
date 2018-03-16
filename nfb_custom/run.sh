af77bbe4f0a7edbdcadf38290a4114784671716b
Friedo <f.barth-github@net-fb.de>
2018-03-16 22:07:05 +0100

    removing module load from run.sh

--- nfb_custom/run.sh
+++ nfb_custom/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -e
+#set -e
 
 # bring up external usb dongle
 hciconfig hci0 up
 
-# initialize zigbee module
-modprobe cp210x
+# add zigbees usb ids to serial module
+# (module is loaded via config.json)
 echo "10c4 89fb" > /sys/module/cp210x/drivers/usb-serial\:cp210x/new_id
 
