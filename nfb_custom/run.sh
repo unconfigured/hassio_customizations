a5978b485dfc007cd5f1562b08faeaf5b4867717
Friedo <f.barth-github@net-fb.de>
2018-03-16 22:32:31 +0100

    zigbee/serial module loading is solely done outside run.sh

--- nfb_custom/run.sh
+++ nfb_custom/run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#set -e
+
+# zigbee is loaded via config.json
+# this is not possible from inside container
 
 # bring up external usb dongle
 hciconfig hci0 up
-
-# add zigbees usb ids to serial module
-# (module is loaded via config.json)
-echo "10c4 89fb" > /sys/module/cp210x/drivers/usb-serial\:cp210x/new_id
 
