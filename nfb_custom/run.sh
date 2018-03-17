1c4ec149026c1786d19861e3d3b6054c55f9cd61
Friedo <f.barth-github@net-fb.de>
2018-03-17 15:39:27 +0100

    mount sys rw bevore insmod

--- nfb_custom/run.sh
+++ nfb_custom/run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+mount -o remount,rw /sys
+
 mkdir -p /tmp/mnt
 mount -o ro /dev/mmcblk0p3 /tmp/mnt
 insmod /tmp/mnt/lib/modules/`uname -r`/kernel/drivers/usb/serial/usbserial.ko
 insmod /tmp/mnt/lib/modules/`uname -r`/kernel/drivers/usb/serial/cp210x.ko
 umount /tmp/mnt
 
-mount -o remount,rw /sys
 echo "${USBID}" > /sys/module/cp210x/drivers/usb-serial\:cp210x/new_id
 
 # bring up external usb dongle
