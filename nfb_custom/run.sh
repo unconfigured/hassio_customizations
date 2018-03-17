4f1bd7161fd82c58f27e1b8a3f8bf85c1b0cf0c1
Friedo <f.barth-github@net-fb.de>
2018-03-17 14:42:40 +0100

    next try for custom usb

--- nfb_custom/run.sh
+++ nfb_custom/run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# zigbee is loaded via config.json
-# this is not possible from inside container
+mkdir /tmp/mnt
+mount -o ro /dev/mmcblk0p3 /tmp/mnt
+insmod /tmp/mnt/lib/modules/`uname -r`/kernel/drivers/usb/serial/usbserial.ko
+insmod /tmp/mnt/lib/modules/`uname -r`/kernel/drivers/usb/serial/cp210x.ko
+umount /tmp/mnt
+
+mount -o remount,rw /sys
+echo "10c4 89fb" > /sys/module/cp210x/drivers/usb-serial\:cp210x/new_id
 
 # bring up external usb dongle
 hciconfig hci0 up
